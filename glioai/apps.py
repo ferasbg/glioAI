a7703012d52f42cb2fb0e2514169825c37c401ad
Feras Baig <ferasbaig@gmail.com>
2020-03-04 10:03:01 -0600

    Update apps.py

--- glioai/apps.py
+++ glioai/apps.py
@@ -14,11 +14,7 @@ class TumorPredictorConfig(AppConfig):
     name = 'glioai'
     path = os.path.join(settings.MODELS, 'mri_tumor.h5')
     model = load_model("/root/brain_tumor/tumorapp/GlioAI/models/mri_tumor.h5")
-    MODEL_PATH = ("/root/brain_tumor/tumorapp/GlioAI/models/mri_tumor.h5")
-    VGG_PATH = ("https://github.com/fchollet/deep-learning-models/''releases/download/v0.1/''vgg16_weights_tf_dim_ordering_tf_kernels_notop.h5")
-    predictor = ImageDataGenerator(model_path = MODEL_PATH/"/root/brain_tumor/tumorapp/GlioAI/models/mri_tumor.h5", 
-                                            pretrained_path = VGG_PATH, 
-                                            multi_label=True)  
+    
 
     
     
